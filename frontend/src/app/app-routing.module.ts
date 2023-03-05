4e1a64b9beb261d434f7818ded0b8cd35e89fb8e
Piri <Piriyangah.Muthurasa@HTW-Berlin.de>
2023-03-05 02:25:44 +0100

    routing, services und bootstrap installiert und eingebunden

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,34 @@
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { AboutComponent } from './about/about.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [ //die Komponente werden eingebunden
+
+//home soll nicht extra route haben sondern direkt aufgerufen werden
+//http://localhost:4200
+{
+  path: " ",
+  component: HomeComponent,
+  pathMatch: 'full' //diese Route nur aufgerufen wird, wenn danach nichts weiter in der URL folgt
+},
+//http://localhost:4200/about
+{
+  path: "about",
+  component: AboutComponent
+},
+//http://localhost:4200/login
+{
+  path: "login",
+  component: LoginComponent
+}
+];
 
 @NgModule({
+  declarations: [],
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: []
 })
 export class AppRoutingModule { }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,17 +6,27 @@ import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AboutComponent } from './about/about.component';
 import { TasksComponent } from './tasks/tasks.component';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { FooterComponent } from './footer/footer.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
     AboutComponent,
-    TasksComponent
+    TasksComponent,
+    NavComponent,
+    HomeComponent,
+    LoginComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
